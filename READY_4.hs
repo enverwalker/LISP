453d202af4f3b87bbe5d3e45fedc73b5fc30281c
enverwalker <48453171+enverwalker@users.noreply.github.com>
2020-05-28 04:46:32 +0300

    Update READY_4.hs

--- READY_4.hs
+++ READY_4.hs
@@ -8,3 +8,9 @@ main =do
   print(listDepth [[1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5]])
   print(listDepth [])
   print(listDepth [[9]])
+-- 12. Определите функцию, разбивающую список (a, b, c, d, ...) на пары ((a, b), (c, d), ...).
+couples :: [Float] -> [[Float]]
+couples [] = []
+couples (a:b:tail) = [a, b]:(couples tail)
+main = do
+print $ (couples [0.1, 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1, 9.1])
