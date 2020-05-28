84224d1c92f073c65f46e95dd1807f25402f9fe0
enverwalker <48453171+enverwalker@users.noreply.github.com>
2020-05-28 04:40:46 +0300

    Update READY_4.hs

--- READY_4.hs
+++ READY_4.hs
@@ -1,1 +1,10 @@
-
+--14. Определите функцию, вычисляющую глубину списка (самой глубокой ветви).
+listDepth :: [[Float]] -> [Float]
+listDepth [] = []
+listDepth ([x]) = x
+listDepth (x:xs) = listDepth xs
+main =do
+  putStrLn "Тесты к задаче №29"
+  print(listDepth [[1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5]])
+  print(listDepth [])
+  print(listDepth [[9]])
